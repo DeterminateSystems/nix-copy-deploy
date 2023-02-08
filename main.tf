397ccb8fdbfb1d28cf9a88ad3db464e738fcc8db
Luc Perkins <lucperkins@gmail.com>
2023-02-08 10:30:45 -0800

    First commit; Nix + Terraform setup

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "1.3.7"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.26.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
