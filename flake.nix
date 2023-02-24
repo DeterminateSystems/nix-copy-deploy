7ac499b9f1b0647e2335c8408558322aeee479ea
Luc Perkins <lucperkins@gmail.com>
2023-02-24 10:42:18 -0800

    Inline notes

--- flake.nix
+++ flake.nix
@@ -1,25 +1,33 @@
 {
-  description = "nix copy deployment example";
+  description = "`nix copy` deployment example";
 
   inputs = {
+    # We'll tie Nixpkgs to a stable release
     nixpkgs.url = "nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            jq
-            openssh
-            shellcheck
-            terraform
-          ];
-        };
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          jq
+          openssh
+          shellcheck
+          terraform
+        ];
+      };
 
-        packages.default = pkgs.ponysay;
-      });
+      # We'll keep it simple by using a package from Nixpkgs, but this could be any package
+      # we want: a web server, a database server, a Bitcoin miner (ew), etc.
+      packages.default = pkgs.ponysay;
+    });
 }
